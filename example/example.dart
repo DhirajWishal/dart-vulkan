1676c5fb2b36d04611ce9d207fa84e5f241b11ad
root.ext <root.ext@gmail.com>
2021-03-03 20:52:17 -0500

    publish 0.1.2

--- example/example.dart
+++ example/example.dart
@@ -1,32 +1,33 @@
 import 'dart:ffi';
 import 'package:ffi/ffi.dart';
-import 'package:ffi_utils/ffi_utils.dart';
 import 'package:vulkan/vulkan.dart';
 
 void main() {
   initVulkan();
 
-  var ai = allocate<VkApplicationInfo>().ref
+  final ai = allocate<VkApplicationInfo>();
+  ai.ref
     ..sType = VK_STRUCTURE_TYPE_APPLICATION_INFO
     ..pNext = nullptr
-    ..pApplicationName = NativeString.fromString('Application')
+    ..pApplicationName = Utf8.toUtf8('Application')
     ..applicationVersion = makeVersion(1, 0, 0)
-    ..pEngineName = NativeString.fromString('Engine')
+    ..pEngineName = Utf8.toUtf8('Engine')
     ..engineVersion = 0
     ..apiVersion = makeVersion(1, 0, 0);
 
-  var ici = allocate<VkInstanceCreateInfo>().ref
+  final ici = allocate<VkInstanceCreateInfo>();
+  ici.ref
     ..sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO
     ..pNext = nullptr
     ..flags = 0
-    ..pApplicationInfo = ai.addressOf
+    ..pApplicationInfo = ai
     ..enabledExtensionCount = 0
     ..ppEnabledExtensionNames = nullptr
     ..enabledLayerCount = 0
     ..ppEnabledLayerNames = nullptr;
 
-  var instance = allocate<Pointer<VkInstance>>();
-  var result = vkCreateInstance(ici.addressOf, nullptr, instance);
+  final instance = allocate<Pointer<VkInstance>>();
+  final result = vkCreateInstance(ici, nullptr, instance);
   print(result == VK_SUCCESS ? 'Vulkan instance succesfully created' : 'Failed to create Vulkan insatnce');
 }
 
